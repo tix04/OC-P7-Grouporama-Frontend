8355a82b54d4c0338567396174b4cb4e7b4fc769
tix04 <04tiananet@gmail.com>
2021-03-31 22:35:43 +0300

    feat:Complete new post Notification Alert

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -2,18 +2,59 @@
 <b-container>
     <b-container>
         <HelloUser msg="Welcome to Grouporama Chat" />
-        <p>You have <span>7</span> new Posts.</p>
+        <p v-if="this.totalPosts > this.viewedPosts">You have <span>{{ newPosts }}</span> new Posts.</p>
+        <p v-else>You have no new Notifications</p>
         <!--Figure out Logic to determine which posts you haven't seen-->
     </b-container>
 </b-container>
 </template>
 
 <script>
-import HelloUser from "./HelloUser.vue"
+import HelloUser from "./HelloUser.vue";
+import axios from 'axios';
 export default {
     name: "Notification",
     components: {
         HelloUser
+    },
+    data () {
+        return {
+            viewedPosts: 0,
+            totalPosts: 0
+
+        }
+    },
+    computed: {
+        newPosts() {
+            return this.totalPosts - this.viewedPosts;
+        }
+    },
+    async created () {
+        try {
+            let postSummary = await axios.get('http://localhost:3000/posts/postCount');
+            
+            
+            this.totalPosts = postSummary.data[0].postsCount;
+            this.viewedPosts = postSummary.data[0].viewed_posts;
+            console.log(this.totalPosts);
+        }catch(err) {
+            console.error(err);
+        }
+
+    },
+    async beforeDestroy () {
+        
+        try {
+            
+            await axios.put('http://localhost:3000/posts/newPostStatus/', {
+                count: this.totalPosts
+            });
+
+            console.log('setting new viewed posts is done')
+
+        }catch(err) {
+            console.error(err);
+        }
     }
 }
 </script>
